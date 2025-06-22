6ca99c8574dd09a1af9e6669fe1965f959a1de91
kaysiz <ksiziva@gmail.com>
2025-06-22 19:27:27 +0200

    Add method to process s3 files and excluded already processed ones

--- packages/server/services/seedSource/seedSource.js
+++ packages/server/services/seedSource/seedSource.js
@@ -3,10 +3,11 @@ const DataCiteEventData = require('./dataCiteEventData')
 const axios = require('../axiosService')
 const CziFile = require('./cziFile')
 const AsapFile = require('./asapFile')
+const EupmcFile = require('./eupmcFile')
 const AwsS3Service = require('../awsS3Service')
 const { model: ActivityLog } = require('../../models/activityLog')
 const { model: Source } = require('../../models/source')
-const fs = require('fs')
+const { model: CziFileModel } = require('../../models/cziFileModel')
 const path = require('path')
 const { parse } = require('csv-parse/sync') // Use csv-parse for parsing CSV files
 
@@ -217,6 +218,43 @@ class SeedSource {
       const czi = new CziFile(files)
 
       return await czi.readSource()
+    } catch (e) {
+      logger.error(e)
+    }
+
+    return false
+  }
+
+  static async createInstanceReadS3Eupmc() {
+    try {
+      const source = await Source.query().findOne({ abbreviation: 'eupmc' });
+
+      if (!source) {
+        throw new Error('Source "eupmc" not found in the database.');
+      }
+
+      const awsService = new AwsS3Service()
+
+      const files = await awsService.readS3Folder(
+        process.env.S3_EUROPEPMC_BUCKET,
+        process.env.S3_EUROPEPMC_FOLDER
+      )
+
+      const filesToProcess = await Promise.all(files.map(async (file) => {
+        const existingFile = await CziFileModel.query().findOne({ file_name: file.fileKey });
+        return { file, isProcessed: existingFile && existingFile.proccessed === true };
+      }));
+
+      const unprocessedFiles = filesToProcess
+        .filter(item => !item.isProcessed)
+        .map(item => item.file);
+
+      // eslint-disable-next-line no-console
+      console.log(`Processing ${unprocessedFiles.length} out of ${files.length} files`);
+
+      const eupmc = new EupmcFile(unprocessedFiles, source.id)
+
+      return await eupmc.readSource()
     } catch (e) {
       logger.error(e)
     }
